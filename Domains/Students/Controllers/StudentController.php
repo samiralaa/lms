cfc15c1e7f5e91bac7f2426c2266e378d09316f1
Samir Alaa <76245712+samiralaa@users.noreply.github.com>
2025-03-08 03:09:52 +0400

    add more

--- Domains/Students/Controllers/StudentController.php
+++ Domains/Students/Controllers/StudentController.php
@@ -1,6 +1,7 @@
 <?php
 namespace Domains\Students\Controllers;
 
+use Domains\Students\Repositories\StudentRepository;
 use Domains\Students\Requests\StudentRegisterRequest;
 use Domains\Students\Services\StudentService;
 
@@ -8,22 +9,21 @@ use Domains\Students\Services\StudentService;
 class StudentController
 {
     protected $studentService;
-
-    public function __construct(StudentService $studentService)
+    protected $studentRerop;
+    public function __construct(StudentService $studentService,StudentRepository $studentRerop)
     {
         $this->studentService = $studentService;
+        $this->studentRerop=$studentRerop;
     }
 
     public function register(StudentRegisterRequest $request)
     {
-        if (!class_exists(\Domains\Students\Requests\StudentRegisterRequest::class)) {
-            dd('StudentRegisterRequest does not exist!');
-        }
-
-
-
         $student = $this->studentService->registerStudent($request->validated());
-
         return response()->json(['message' => 'Student registered successfully', 'student' => $student], 201);
+    }
+    public function allStudentsToDashboard()
+    {
+        return $this->studentRerop->getAllStudents();
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::post('/test', [\Domains\Students\Controllers\StudentController::class, 'register']);
+Route::post('/create-students', [\Domains\Students\Controllers\StudentController::class, 'register']);
+Route::get('/all-students', [\Domains\Students\Controllers\StudentController::class, 'allStudentsToDashboard']);
+
+
+
